4649ff23a2f1c5ad52c707e9397d9d755c2948f1
Sam <s.drobakha@gmail.com>
2018-11-04 14:06:21 +0200

    refactoring done

--- misc/SFTP-CLOUD.aws-cloudformation-masterfile.sh
+++ misc/SFTP-CLOUD.aws-cloudformation-masterfile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#set pass for user <ftpuser1> here
+sftp_pass=ftppass1
+
 #this will create stack (similar command to update stack, see man)
 aws cloudformation create-stack \
 --stack-name gSFTP-CLOUD \
@@ -12,8 +15,10 @@ aws cloudformation create-stack \
 aws cloudformation delete-stack --stack-name gSFTP-CLOUD
 
 #this will take params for ansible
-export S3_BUCKET_ID=$(aws cloudformation describe-stacks --stack-name gSFTP-CLOUD --output text | grep S3bucketID | awk {'print $3'})
+export SFTP_S3_BUCKET_ID=$(aws cloudformation describe-stacks --stack-name gSFTP-CLOUD --output text | grep S3bucketID | awk {'print $3'})
 export PUBLIC_IP=$(aws cloudformation describe-stacks --stack-name gSFTP-CLOUD --output text | grep PublicIPAddress | awk {'print $3'})
 export SFTP_DNS=$(aws cloudformation describe-stacks --stack-name gSFTP-CLOUD --output text | grep DomainName | awk {'print $3'})
 
-
+#this will generate password for SFtP user <ftpuser1>
+#usage: openssl passwd -salt <salt> -1 <password>
+export SFTP_PASS_HASH=$(openssl passwd -salt 990 -1 $sftp_pass)
